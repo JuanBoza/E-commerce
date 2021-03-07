2f118044e775e467cd7831c3175cf702d8252efe
Juan Boza <Juanboza80@gmail.com>
2021-03-07 13:27:54 -0700

    columns added to product.js

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true
+    },
+    product_name:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    stock:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      defaultValue:20,
+      validate:{
+        isNumeric:true,
+      },
+    price: {
+      type:DataTypes.DECIMAL,
+      allowNull:false,
+      validate:{
+        isDecimal:true,
+      },
+    category_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:'category',
+        key:'id'
+      }
+    }
+    }
+
+    }
   },
   {
     sequelize,
